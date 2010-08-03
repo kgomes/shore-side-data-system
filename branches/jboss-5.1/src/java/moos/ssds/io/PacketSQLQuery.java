4be7a0f9868dd53598645082a8a2176a10b869ae
kgomes <kgomes@e563c906-e84c-11dd-9b7c-bbba4c9295f9>
2010-08-03 18:08:23 +0000

    Cleaned up the logging and removed any remnants of the direct connection using properties.  I have taken the view point that the entity utilizing this class should provide the DataSource (sort of in the dependency injection camp).

--- branches/jboss-5.1/src/java/moos/ssds/io/PacketSQLQuery.java
+++ branches/jboss-5.1/src/java/moos/ssds/io/PacketSQLQuery.java
@@ -1,6 +1,18 @@
-// $Header: /home/cvs/ssds/src/java/moos/ssds/io/PacketInput.java,v 1.20
-// 2005/04/25 15:32:02 kgomes Exp $
-
+/*
+ * Copyright 2009 MBARI
+ *
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 2.1 
+ * (the "License"); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.gnu.org/copyleft/lesser.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package moos.ssds.io;
 
 import java.io.ByteArrayOutputStream;
@@ -16,6 +28,11 @@ import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * @author kgomes
+ * 
+ */
 public class PacketSQLQuery implements Enumeration<byte[]> {
 
 	/** A log4j logger */
@@ -32,30 +49,17 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 	 */
 	private DataSource dataSource = null;
 
-	/* ********************************************** */
-	/* These parameters are for direct DB connections */
-	/* ********************************************** */
-	// private String databaseJDBCUrl = null;
-	// private String username = null;
-	// private String password = null;
-	// This is a boolean that tells the object if this is supposed to be a
-	// direct connection to the database or not
-	// private boolean directConnection = false;
-	/* ************************ */
-	/* End direct DB connection */
-	/* ************************ */
-
 	/**
 	 * This is the counter for which row in the cache we are on. This means that
 	 * this row has already been returned through the nextElement() call.
 	 */
-	private int cacheRow = 0;
+	private int cacheRow = -1;
 
 	/**
 	 * This is the counter for which row in the overall result set we are on.
 	 * This means this row has already been returned from a nextElement() call.
 	 */
-	private int resultSetRow = 0;
+	private int resultSetRow = -1;
 
 	/**
 	 * This is the size of the array of byte arrays that will be used to cache
@@ -80,47 +84,13 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 		logger.debug("PacketSQLQuery constructor called with DataSource = "
 				+ dataSource + " and PacketSQLQueryFactory = "
 				+ packetSQLQueryFactory);
+		logCurrentState();
 		// Call the method to set the data sources
 		this.setDataSource(dataSource);
 		this.packetSQLQueryFactory = packetSQLQueryFactory;
+		logger.debug("Data Source set and factory associated");
+		logCurrentState();
 	}
-
-	/**
-	 * This is the constructor that takes in several strings to setup a database
-	 * connection. This is for times when you want to use the
-	 * <code>PacketSQLInput</code> outside of a J2EE container.
-	 * 
-	 * @param databaseDriverClassName
-	 * @param databaseJDBCUrl
-	 * @param username
-	 * @param password
-	 * @param deviceID
-	 * @throws ClassNotFoundException
-	 */
-	// public PacketSQLQuery(String databaseDriverClassName,
-	// String databaseJDBCUrl, String username, String password,
-	// PacketSQLQueryFactory packetSQLQueryFactory) throws SQLException,
-	// ClassNotFoundException {
-	// // First check that incoming parameters are OK
-	// if ((databaseDriverClassName == null) || (databaseJDBCUrl == null)
-	// || (username == null) || (password == null))
-	// throw new SQLException("One of the constructor parameters "
-	// + "was not specified, all four must be.");
-	//
-	// // Set the flag for a direct connection
-	// this.directConnection = true;
-	//
-	// // Set the local variabless
-	// this.databaseJDBCUrl = databaseJDBCUrl;
-	// this.username = username;
-	// this.password = password;
-	//
-	// // Load the DB driver
-	// Class.forName(databaseDriverClassName);
-	//
-	// // Set the PacketSQLFactory
-	// this.packetSQLQueryFactory = packetSQLQueryFactory;
-	// }
 
 	/**
 	 * @return Returns the ds.
@@ -137,62 +107,9 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 	 * @param dataSource
 	 */
 	private void setDataSource(DataSource dataSource) throws SQLException {
-		// Set the flag to turn off the direct connection
-		// this.directConnection = false;
-		// If the data source is null, look up one using the local properties
+		// If the data source is null, throw an Exception
 		if (dataSource == null) {
 			throw new SQLException("No DataSource specified.");
-			// logger.debug("No DataSource specified, will "
-			// + "construct one from the io.properties");
-			// Properties ioProperties = null;
-			// // Create and load the io properties
-			// ioProperties = new Properties();
-			// try {
-			// ioProperties.load(this.getClass().getResourceAsStream(
-			// "/moos/ssds/io/io.properties"));
-			// } catch (Exception e) {
-			// logger.error("Exception trying to read in properties file: "
-			// + e.getMessage());
-			// }
-			// // Grab JNDI stuff
-			// String jndiHostName = ioProperties
-			// .getProperty("io.storage.sql.jndi.server.name");
-			// String dataSourceJndiName = "java:/"
-			// + ioProperties.getProperty("io.storage.sql.jndi.name");
-			// logger.debug("jndiHostName = " + jndiHostName
-			// + ", dataSourceJndiName = " + dataSourceJndiName);
-			//
-			// // Now grab the DataSource from the JNDI
-			// Context jndiContext = null;
-			// try {
-			// jndiContext = new InitialContext();
-			// if ((jndiHostName != null) && (!jndiHostName.equals(""))) {
-			// jndiContext.removeFromEnvironment(Context.PROVIDER_URL);
-			// jndiContext.addToEnvironment(Context.PROVIDER_URL,
-			// jndiHostName + ":1099");
-			// }
-			// } catch (NamingException ne) {
-			// logger
-			// .error("!!--> A naming exception was caught while trying "
-			// + "to get an initial context: "
-			// + ne.getMessage());
-			// return;
-			// } catch (Exception e) {
-			// logger
-			// .error("!!--> An unknown exception was caught while trying "
-			// + "to get an initial context: "
-			// + e.getMessage());
-			// return;
-			// }
-			// try {
-			// this.dataSource = (DataSource) jndiContext
-			// .lookup(dataSourceJndiName);
-			// logger.debug("DataSource from JNDI DataSource = "
-			// + this.dataSource);
-			// } catch (NamingException e1) {
-			// logger.error("Could not get DataSource: " + e1.getMessage());
-			// }
-
 		} else {
 			this.dataSource = dataSource;
 		}
@@ -204,6 +121,7 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 	 */
 	public void queryForData() throws SQLException {
 		logger.debug("queryForData called");
+		logCurrentState();
 		// Make sure the factory is there and has a device ID
 		if (packetSQLQueryFactory == null
 				|| packetSQLQueryFactory.getDeviceID() <= 0)
@@ -222,6 +140,8 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 
 		// Call the method to fill the results cache
 		fillResultsCache();
+		logger.debug("query for data done");
+		logCurrentState();
 	}
 
 	/**
@@ -234,11 +154,6 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 
 		// Create a connection to the database
 		Connection connection = null;
-
-		// Grab a new connection
-		// if (!directConnection) {
-		logger.debug("It is not a direct connection so we "
-				+ "will grab a connection from the DataSource");
 		try {
 			connection = this.dataSource.getConnection();
 		} catch (Exception e) {
@@ -246,17 +161,6 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 					+ "Connection from DataSource "
 					+ "(not direction connection): " + e.getMessage());
 		}
-		// } else {
-		// logger.debug("This is a direct connection, so we "
-		// + "will use DriverManager to get a connection");
-		// try {
-		// connection = DriverManager.getConnection(this.databaseJDBCUrl,
-		// this.username, this.password);
-		// } catch (Exception e) {
-		// logger.error("Exception caught trying to get "
-		// + "Connection from DataSource: " + e.getMessage());
-		// }
-		// }
 
 		// Grab the SQL statement from the factory
 		String queryString = packetSQLQueryFactory.getQueryStatement();
@@ -310,13 +214,11 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 
 				// Now iterate over the number of rows and fill the cache
 				for (int i = 0; i < cacheSize; i++) {
-					logger.debug("On page row " + i);
+					logCurrentState();
 					try {
 						// Advance cursor and see if there is something to
 						// return
 						if (resultSet.next()) {
-							logger.debug("OK, next record found, "
-									+ "going to read in the columns");
 							// Reset the byte array outputstream
 							byteArrayOutputStream.reset();
 
@@ -324,22 +226,12 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 							// PacketSQLQueryFactory
 							for (int j = 0; j < packetSQLQueryFactory
 									.listReturnFields().length; j++) {
-								logger.debug("Reading field "
-										+ j
-										+ " which should be called "
-										+ packetSQLQueryFactory
-												.listReturnFields()[j]
-										+ " and should be of type "
-										+ packetSQLQueryFactory
-												.listReturnClasses()[j]);
 								try {
 									if (packetSQLQueryFactory
 											.listReturnClasses()[j] == int.class) {
 										Integer integerResult = resultSet
 												.getInt(packetSQLQueryFactory
 														.listReturnFields()[j]);
-										logger.debug("Read integer of "
-												+ integerResult);
 										dataOutputStream
 												.writeInt(integerResult);
 									} else if (packetSQLQueryFactory
@@ -347,17 +239,12 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 										Long longResult = resultSet
 												.getLong(packetSQLQueryFactory
 														.listReturnFields()[j]);
-										logger.debug("Read long of "
-												+ longResult);
 										dataOutputStream.writeLong(longResult);
 									} else if (packetSQLQueryFactory
 											.listReturnClasses()[j] == byte[].class) {
 										byte[] byteResult = resultSet
 												.getBytes(packetSQLQueryFactory
 														.listReturnFields()[j]);
-										logger
-												.debug("Read bytes (as String) of "
-														+ new String(byteResult));
 										dataOutputStream.write(byteResult);
 									}
 								} catch (IOException e) {
@@ -371,23 +258,21 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 							// stuff it into the page cache
 							resultsCache[i] = byteArrayOutputStream
 									.toByteArray();
-							logger
-									.debug("Wrote result array to results cache at index "
-											+ i);
 						} else {
 							logger.debug("Looks like there is "
 									+ "no next() record.");
 							resultsCache[i] = null;
 						}
 					} catch (SQLException e) {
-						logger
-								.error("SQLException caught trying to readObject: "
-										+ e.getMessage());
+						logger.error("SQLException caught "
+								+ "trying to readObject: " + e.getMessage());
 					}
 				}
 			}
 			// Reset the cache row counter
 			cacheRow = -1;
+			logger.debug("Done filling rows");
+			logCurrentState();
 		} else {
 			logger.error("ResultSet was NULL!");
 		}
@@ -413,6 +298,8 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 		} catch (Exception e) {
 			logger.error("Exception caught trying to close: " + e.getMessage());
 		}
+		logger.debug("Done with fillResultsCache");
+		logCurrentState();
 	}
 
 	/**
@@ -486,9 +373,6 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 	 */
 	public byte[] nextElement() {
 
-		logger.debug("nextElement called, cacheRow = " + cacheRow
-				+ " and resultSetRow = " + resultSetRow);
-
 		// Check if the cache needs refreshing
 		checkIfCacheNeedsRefresh();
 
@@ -522,6 +406,22 @@ public class PacketSQLQuery implements Enumeration<byte[]> {
 						+ "refresh the cache: " + e.getMessage());
 			}
 		}
+	}
+
+	private void logCurrentState() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("PacketSQLQuery State:cacheRow = " + cacheRow
+				+ ";resultsSetRow = " + resultSetRow);
+		sb.append(";ResultsCache Vector = [");
+		for (int i = 0; i < cacheSize; i++) {
+			if (resultsCache[i] == null) {
+				sb.append("0 ");
+			} else {
+				sb.append("1 ");
+			}
+		}
+		sb.append("]");
+		logger.debug(sb.toString());
 	}
 
 	/**
