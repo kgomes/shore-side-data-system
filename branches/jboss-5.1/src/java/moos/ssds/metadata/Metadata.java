977175165956cd77ae5540a5b8955634c79ef028
kgomes <kgomes@e563c906-e84c-11dd-9b7c-bbba4c9295f9>
2011-03-17 00:14:02 +0000

    Added more top level metadata objects for help in marshalling

--- branches/jboss-5.1/src/java/moos/ssds/metadata/Metadata.java
+++ branches/jboss-5.1/src/java/moos/ssds/metadata/Metadata.java
@@ -1,6 +1,7 @@
 package moos.ssds.metadata;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * This is largely a convenience class for the XML<->Java translation. Since XML
@@ -12,13 +13,29 @@ import java.util.Collection;
  */
 public class Metadata {
 
+	private Collection<DataContainer> dataContainers = null;
+
+	private Collection<DataContainerGroup> dataContainerGroups = null;
+
+	private Collection<DataProducer> dataProducers = null;
+
+	private Collection<DataProducerGroup> dataProducerGroups = null;
+
+	private Collection<DataProducer> deployments = null;
+
+	private Collection<Device> devices = null;
+
 	private Collection<Event> events = null;
 
 	private Collection<Keyword> keywords = null;
 
 	private Collection<Person> persons = null;
 
+	private Collection<DataProducer> processRuns = null;
+
 	private Collection<Resource> resources = null;
+
+	private Collection<Software> softwares = null;
 
 	private Collection<StandardDomain> standardDomains = null;
 
@@ -29,6 +46,56 @@ public class Metadata {
 	private Collection<StandardUnit> standardUnits = null;
 
 	private Collection<StandardVariable> standardVariables = null;
+
+	public Collection<DataContainer> getDataContainers() {
+		return dataContainers;
+	}
+
+	public void setDataContainers(Collection<DataContainer> dataContainers) {
+		this.dataContainers = dataContainers;
+	}
+
+	public Collection<DataContainerGroup> getDataContainerGroups() {
+		return dataContainerGroups;
+	}
+
+	public void setDataContainerGroups(
+			Collection<DataContainerGroup> dataContainerGroups) {
+		this.dataContainerGroups = dataContainerGroups;
+	}
+
+	public Collection<DataProducer> getDataProducers() {
+		return dataProducers;
+	}
+
+	public void setDataProducers(Collection<DataProducer> dataProducers) {
+		this.dataProducers = dataProducers;
+	}
+
+	public Collection<DataProducerGroup> getDataProducerGroups() {
+		return dataProducerGroups;
+	}
+
+	public void setDataProducerGroups(
+			Collection<DataProducerGroup> dataProducerGroups) {
+		this.dataProducerGroups = dataProducerGroups;
+	}
+
+	public Collection<DataProducer> getDeployments() {
+		return deployments;
+	}
+
+	public void setDeployments(Collection<DataProducer> deployments) {
+		this.deployments = deployments;
+	}
+
+	public Collection<Device> getDevices() {
+		return devices;
+	}
+
+	public void setDevices(Collection<Device> devices) {
+		this.devices = devices;
+	}
 
 	public Collection<Event> getEvents() {
 		return events;
@@ -54,12 +121,28 @@ public class Metadata {
 		this.persons = persons;
 	}
 
+	public Collection<DataProducer> getProcessRuns() {
+		return processRuns;
+	}
+
+	public void setProcessRuns(Collection<DataProducer> processRuns) {
+		this.processRuns = processRuns;
+	}
+
 	public Collection<Resource> getResources() {
 		return resources;
 	}
 
 	public void setResources(Collection<Resource> resources) {
 		this.resources = resources;
+	}
+
+	public Collection<Software> getSoftwares() {
+		return softwares;
+	}
+
+	public void setSoftwares(Collection<Software> softwares) {
+		this.softwares = softwares;
 	}
 
 	public Collection<StandardDomain> getStandardDomains() {
@@ -104,4 +187,43 @@ public class Metadata {
 		this.standardVariables = standardVariables;
 	}
 
+	public String toStringRepresentation(String delimeter) {
+		StringBuilder messageBuilder = new StringBuilder();
+		if (dataContainers != null && dataContainers.size() > 0) {
+			for (Iterator<DataContainer> iterator = dataContainers.iterator(); iterator
+					.hasNext();) {
+				messageBuilder.append(iterator.next().toStringRepresentation(
+						delimeter)
+						+ "\n");
+			}
+		}
+		if (dataContainerGroups != null && dataContainerGroups.size() > 0) {
+			for (Iterator<DataContainerGroup> iterator = dataContainerGroups
+					.iterator(); iterator.hasNext();) {
+				messageBuilder.append(iterator.next().toStringRepresentation(
+						delimeter)
+						+ "\n");
+
+			}
+		}
+		if (dataProducers != null && dataProducers.size() > 0) {
+			for (Iterator<DataProducer> iterator = dataProducers.iterator(); iterator
+					.hasNext();) {
+				messageBuilder.append(iterator.next().toStringRepresentation(
+						delimeter)
+						+ "\n");
+
+			}
+		}
+		if (dataProducerGroups != null && dataProducerGroups.size() > 0) {
+			for (Iterator<DataProducerGroup> iterator = dataProducerGroups
+					.iterator(); iterator.hasNext();) {
+				messageBuilder.append(iterator.next().toStringRepresentation(
+						delimeter)
+						+ "\n");
+
+			}
+		}
+		return messageBuilder.toString();
+	}
 }
